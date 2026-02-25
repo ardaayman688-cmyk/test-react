580efbe3a5d7e898947957c2a9f8753caa5accaf
ardaayman688-cmyk <https://github.com/ardaayman688-cmyk/test-react.git>
2026-02-25 20:19:34 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import './App.css';
+function App() {
+ const [tasks, setTasks] = useState(["Eat Breakfast", "Take a shower", "Walk the dog"]); 
+ const [newTask, setNewTask] = useState("");
+ function handleInputChange(event) { setNewTask(event.target.value); } 
+ function addTask() { if (newTask.trim() !== "") { setTasks([...tasks, newTask]); setNewTask(""); } } 
+  function deleteTask(index) { const updatedTasks = tasks.filter((_, i) => i !== index); setTasks(updatedTasks); }
+return (
+ <div className='todo'>
+ <h1>To-Do-List</h1>
+ <div>
+  <input type="text" placeholder='Enter a task..' value={newTask} onChange={handleInputChange} />
+  <button className='add-b' onClick={addTask}> Add </button>
+  </div>
+  <ol> {tasks.map((task, index) => (
+  <li key={index}>
+  <span className='text'>{task}</span>
+  <button className='delete' onClick={() => deleteTask(index)}> Delete </button> 
+  </li> ))}
+  </ol>
+  </div>
+    ); }
+     export default App;
