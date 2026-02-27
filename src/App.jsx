e43df42c6846e9dfcbc0c0d578e78f50c3bf9699
ardaayman688-cmyk <https://github.com/ardaayman688-cmyk/test-react.git>
2026-02-27 21:30:55 +0200

    llkl

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
 import './App.css';
+import { LiaAdSolid } from "react-icons/lia";
+import { MdDeleteForever } from "react-icons/md";
+import {Motion} from 'framer-motion'
 function App() {
  const [tasks, setTasks] = useState(["Eat Breakfast", "Take a shower", "Walk the dog"]); 
  const [newTask, setNewTask] = useState("");
@@ -7,18 +10,21 @@ function App() {
  function addTask() { if (newTask.trim() !== "") { setTasks([...tasks, newTask]); setNewTask(""); } } 
   function deleteTask(index) { const updatedTasks = tasks.filter((_, i) => i !== index); setTasks(updatedTasks); }
 return (
+
  <div className='todo'>
  <h1>To-Do-List</h1>
  <div>
   <input type="text" placeholder='Enter a task..' value={newTask} onChange={handleInputChange} />
-  <button className='add-b' onClick={addTask}> Add </button>
+  <button className='add-b' onClick={addTask} > <LiaAdSolid size={38} /> </button>
   </div>
   <ol> {tasks.map((task, index) => (
   <li key={index}>
   <span className='text'>{task}</span>
-  <button className='delete' onClick={() => deleteTask(index)}> Delete </button> 
+  <button className='delete' onClick={() => deleteTask(index)}> Delete <MdDeleteForever size={17} /> </button> 
   </li> ))}
   </ol>
   </div>
+ 
+
     ); }
      export default App;
